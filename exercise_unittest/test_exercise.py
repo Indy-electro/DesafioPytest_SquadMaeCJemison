54202b44f43dd052020b32ad450987cd1091146a
MildePaula <millenadepaula@gmail.com>
2024-03-08 19:33:53 -0300

    Exercicio 1 função

---
+++ exercise_unittest/test_exercise.py
@@ -0,0 +1,14 @@
+import unittest
+
+def str_to_bool(value):
+    try:
+        value = value.lower()
+    except AttributeError:
+         raise AttributeError(f"{value} must be of type string")    
+    true_values = ['y','yes']
+    false_values = ['no', 'n']
+
+    if value in true_values:
+        return True
+    if value in false_values:
+        return False
